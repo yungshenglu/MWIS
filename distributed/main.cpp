6c6e1b5333d8a75f61b1dc426a6e81fcc6d2bb8f
yungshenglu <yungshenglu1994@gmail.com>
2017-11-14 14:40:53 +0800

    Add the counts of each run's result and percentage

--- distributed/main.cpp
+++ distributed/main.cpp
@@ -3,6 +3,14 @@
 
 using namespace std;
 
+typedef struct result {
+    vector<int> result_list;
+    int count;
+    int weight;
+} Result;
+
+vector<Result> result;
+
 bool compare_result(vector<bool> latest, vector<bool> current) {
     for (int i = 0; i < latest.size(); ++i) {
         if (latest[i] != current[i])
@@ -12,15 +20,45 @@ bool compare_result(vector<bool> latest, vector<bool> current) {
     return true;
 }
 
+void store_result(vector<int> current, int weight) {
+    bool isSameWithLatest = true;
+    Result latest;
+
+    if (result.empty()) {
+        latest.result_list.assign(current.begin(), current.end());
+        latest.count = 1;
+        latest.weight = weight;
+        result.push_back(latest);
+        return;
+    }
+
+    for (int i = 0; i < result.size(); ++i) {
+        for (int j = 0; j < result[i].result_list.size(); ++j) {
+            if (current[j] != result[i].result_list[j]) {
+                latest.result_list.assign(current.begin(), current.end());
+                latest.count = 1;
+                latest.weight = weight;
+                result.push_back(latest);
+                isSameWithLatest = false;
+                break;
+            }
+        }
+
+        if (isSameWithLatest) {
+            result[i].count += 1;
+        }
+    }
+}
+
 int main(int argc, char *argv[]) {
     ifstream fin;
-    vector<bool> latest, result;
+    vector<bool> latest, current;
     vector<int> tmp_latest;
     int vertex, MWIS_weight = 0;
     char *filename = argv[1];
     int simulation_times = 1000;
     bool isSame = true;
-    
+
     // Read the input file
     fin.open(filename);
     while (!fin.eof()) {
@@ -28,7 +66,7 @@ int main(int argc, char *argv[]) {
         vector<MWIS> mwis(vertex);
 
         latest.resize(vertex, true);
-        result.resize(vertex, false);
+        current.resize(vertex, false);
 
         for (int i = 0; i < mwis.size(); ++i) {
             mwis[i].set_weight(fin);
@@ -46,9 +84,9 @@ int main(int argc, char *argv[]) {
         }
         
         for (int t = 0; t < simulation_times; ++t) {
-            while (!compare_result(latest, result)) {
-                for (int i = 0; i < result.size(); ++i)
-                    latest[i] = result[i];
+            while (!compare_result(latest, current)) {
+                for (int i = 0; i < current.size(); ++i)
+                    latest[i] = current[i];
                 
                 for (int j = 0; j < mwis.size(); ++j) {
                     // Calculate the degree and priority for own vertex.
@@ -65,14 +103,14 @@ int main(int argc, char *argv[]) {
                 for (int j = 0; j < mwis.size(); ++j) {
                     // Receive all msg from own neighbors.
                     mwis[j].recv_msg();
-                    result[j] = mwis[j].get_map()[j].isMWIS;
+                    current[j] = mwis[j].get_map()[j].isMWIS;
                 }
             }
 
             MWIS_weight = 0;
             vector<int> tmp;
-            for (int i = 0; i < result.size(); ++i) {
-                if (result[i]) {
+            for (int i = 0; i < current.size(); ++i) {
+                if (current[i]) {
                     tmp.push_back(i);
                     MWIS_weight += mwis[i].get_weight();
                 }
@@ -81,11 +119,12 @@ int main(int argc, char *argv[]) {
             isSame = true;
             if (t == 0) {
                 tmp_latest.assign(tmp.begin(), tmp.end());
+                store_result(tmp, MWIS_weight);
             } else {
+                store_result(tmp, MWIS_weight);
                 for (int i = 0; i < tmp.size(); ++i) {
                     if (tmp[i] != tmp_latest[i]) {
                         isSame = false;
-                        break;
                     }
                 }
                 tmp_latest.assign(tmp.begin(), tmp.end());
@@ -94,17 +133,33 @@ int main(int argc, char *argv[]) {
 
         // Print reuslt
         if (isSame) {
-            printf("All %d times results are same.\n", simulation_times);
-            printf("MWIS: {");
-            for (int i = 0; i < tmp_latest.size(); ++i) {
-                if (i == 0) {
-                    printf("%d", tmp_latest[i]);
-                } else {
-                    printf(", %d", tmp_latest[i]);
+            printf("All %d times results are the same.\n", simulation_times);
+            for (int i = 0; i < result.size(); ++i) {
+                printf("MWIS: {");
+                for (int j = 0; j < result[i].result_list.size(); ++j) {
+                    if (j == 0) {
+                        printf("%d", result[i].result_list[j]);
+                    } else {
+                        printf(", %d", result[i].result_list[j]);
+                    }
                 }
+                printf("}, counts: %d, perc: %lf\n", result[i].count, (double)(result[i].count / simulation_times));
+                printf("Total MWIS weight: %d\n", result[i].weight);
             }
-            printf("}\n");
-            printf("Total MWIS weight: %d\n", MWIS_weight);
+        } else {
+            printf("All %d times results have some different.\n", simulation_times);
+            for (int i = 0; i < result.size(); ++i) {
+                printf("MWIS: {");
+                for (int j = 0; j < result[i].result_list.size(); ++j) {
+                    if (j == 0) {
+                        printf("%d", result[i].result_list[j]);
+                    } else {
+                        printf(", %d", result[i].result_list[j]);
+                    }
+                }
+                printf("}, counts: %d, perc: %lf\n", result[i].count, (double)(result[i].count / simulation_times));
+                printf("Total MWIS weight: %d\n", result[i].weight);
+            }
         }
     }
 
